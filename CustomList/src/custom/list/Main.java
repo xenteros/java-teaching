5bd9768a5be6076296cdaf8281ea7efb321384dc
xenteros <Gurgul94>
2017-06-26 20:51:43 +0200

    Custom list implementation

---
+++ CustomList/src/custom/list/Main.java
@@ -0,0 +1,18 @@
+package custom.list;
+
+import custom.list.model.MyList;
+
+/**
+ * Created by agurgul on 26.06.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MyList presidentsOfTheUSA = new MyList("Barack Obama");
+        presidentsOfTheUSA.add("Donald Trump");
+        presidentsOfTheUSA.print();
+        System.out.println();
+        presidentsOfTheUSA.add("Ronald Regan", 1);
+        presidentsOfTheUSA.print();
+    }
+}
---
+++ CustomList/src/custom/list/model/MyList.java
@@ -0,0 +1,60 @@
+package custom.list.model;
+
+/**
+ * Created by agurgul on 26.06.2017.
+ */
+public class MyList {
+
+    private String value;
+    private MyList nextElement;
+
+    public MyList(String value) {
+        this.value = value;
+    }
+
+    public void add(String s) {
+        if (nextElement == null) {
+            nextElement = new MyList(s);
+        } else {
+            nextElement.add(s);
+        }
+    }
+
+    public void add(String s, int i) {
+        if (i > 0) {
+            nextElement.add(s, --i);
+        } else {
+            MyList next = new MyList(this.value);
+            next.setNextElement(this.nextElement);
+
+            this.nextElement = next;
+            this.value = s;
+        }
+    }
+
+
+    public void print() {
+        System.out.println(value);
+        if (nextElement != null) {
+            nextElement.print();
+        }
+    }
+
+
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public MyList getNextElement() {
+        return nextElement;
+    }
+
+    public void setNextElement(MyList nextElement) {
+        this.nextElement = nextElement;
+    }
+}
